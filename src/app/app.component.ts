e1b635f7c862947492652ef92465225b15c4b62a
Dimaroca <dimateoroca@gmail.com>
2023-05-03 12:30:12 -0600

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Tarjeta{
+  titulo: string;
+  subtitulo: string;
+  image: string;
+}
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Ejercicio1';
+export class AppComponent implements OnInit {
+  title = 'Mi primer app en angular';
+  public ArregloTarjeta : Tarjeta []=[];
+
+  ngOnInit():void{//Inicializar cuando se ejecuta la página
+  this.ArregloTarjeta = [
+    {titulo: 'Video 1', subtitulo: 'Subtitulo de video', image: "./assets/descarga.jpg"},
+    {titulo: 'Video 2', subtitulo: 'Subtitulo de video', image: "./assets/im2.jpg"},
+    {titulo: 'Video 3', subtitulo: 'Subtitulo de video', image: "./assets/im3.jpg"},
+    {titulo: 'Video 4', subtitulo: 'Subtitulo de video', image: "./assets/im4.jpg"},
+    {titulo: 'Video 5', subtitulo: 'Subtitulo de video', image: "./assets/im5.jpg"},
+  ]
+}
 }
